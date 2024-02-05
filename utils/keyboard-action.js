2182a1030c239fb76b33e4a439f050a96fad86c3
flyc <yayayahahahaooii@gmail.com>
2024-02-06 01:36:39 +0800

    write some comments

--- utils/keyboard-action.js
+++ utils/keyboard-action.js
@@ -1,9 +1,11 @@
 import { keyboardAction } from './keyboard-control.js'
 
+// TODO 這裡的 key 都是錯的，要改成對的
+
 // 按下 Enter 鍵
 function pressEnter() {
   // keyboardAction('VK_RETURN', 'click')
-  keyboardAction(0x0D, 'click')
+  keyboardAction(0x0d, 'click')
 }
 
 // 按下 Alt 鍵
