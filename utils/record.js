68d2cf37cf9904e66974b3f83543b7b13ac50e72
flyc <yayayahahahaooii@gmail.com>
2024-02-11 02:10:55 +0800

    sorting recorded list

--- utils/record.js
+++ utils/record.js
@@ -1,15 +1,44 @@
 import { GlobalKeyboardListener } from 'node-global-key-listener'
+import fs from 'fs'
+
 const v = new GlobalKeyboardListener()
 
 function start() {
+  const things = []
+
   //Log every key that's pressed.
-  const globalListener = function (e, down) {
+  const globalListener = function (e /*, down: 當前按壓著的案件 map */) {
     console.log(`${e.name}, ${e.state == 'DOWN' ? 'DOWN' : 'UP  '}, [${e.rawKey._nameRaw}]`)
-    console.log('down: ', down)
-    console.log()
+
+    const {
+      name,
+      state,
+      rawKey: { _nameRaw: nameRaw },
+    } = e
+    things.push({
+      name,
+      state,
+      nameRaw,
+      timestamp: Date.now(),
+    })
   }
   v.addListener(globalListener)
-  // v.removeListener(globalListener)
+
+  setTimeout(() => {
+    v.removeListener(globalListener)
+
+    console.log("all these things that Iv'e done: ", things)
+    const miniTimestamp = Math.min(...things.map((thing) => thing.timestamp))
+    for (let index = 0; index < things.length; index++) {
+      const thing = things[index]
+
+      thing.timestamp -= miniTimestamp
+
+      // TODO 避免重複按壓造成多次記錄一樣的事件
+    }
+
+    fs.writeFileSync('./result.json', JSON.stringify(things, null, 2))
+  }, 5000)
 }
 
 start()
