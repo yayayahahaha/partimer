444945d8adb00b6d86e08a55efab71e00754405d
flyc <yayayahahahaooii@gmail.com>
2024-02-11 02:27:15 +0800

    add example of detect next item

--- utils/record.js
+++ utils/record.js
@@ -35,6 +35,38 @@ function start() {
       thing.timestamp -= miniTimestamp
 
       // TODO 避免重複按壓造成多次記錄一樣的事件
+
+      /*
+
+const to = 4
+const list = [0].reduce((payload, _, index) => {
+    while(payload.number <= to) {
+        payload.list.push(new Array(payload.number).fill('number-'+ payload.number))
+        payload.number++
+    }
+    return payload.list
+}, { number:1, list: [] }).flat()
+
+for (let index = 0; index < list.length; index++) {
+    const item = list[index]
+    console.log(item, index, list.length)
+
+    const offset = 1
+    let nextIndex = index + offset
+    let nextItem = list[nextIndex]
+    while(nextItem != null) {
+        console.log('nextItem: ',nextItem)
+        if (nextItem === item) {
+            list.splice(nextIndex, 1)
+        } else break
+        
+        nextIndex = index  + offset
+        nextItem = list[nextIndex]
+    }
+}
+list
+
+      */
     }
 
     fs.writeFileSync('./result.json', JSON.stringify(things, null, 2))
