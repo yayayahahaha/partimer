f17d574e690b9f088be37ca17c720ba645385728
flyc <yayayahahahaooii@gmail.com>
2024-02-16 19:44:03 +0800

    update

--- utils/replay.js
+++ utils/replay.js
@@ -20,9 +20,12 @@ const keyMap = {
   VK_HOME: 'home',
   VK_UP: 'up',
   VK_V: 'v',
+  VK_E: 'e',
   VK_T: 't',
   VK_W: 'w',
   VK_Y: 'y',
+  VK_DELETE: 'delete',
+  VK_NEXT: 'pagedown',
 }
 
 const attackMap = {
@@ -56,15 +59,18 @@ function replayStar(type = THUNDER_STAR) {
     resolveFunction = resolve
   })
 
-  const v = new GlobalKeyboardListener()
-
   // 讀取歷程檔案
-  const fileName = type
-  const file = fs.readFileSync(path.resolve(fileName), 'utf8')
-  const steps = JSON.parse(file)
+  let steps = null
+  if (typeof type === 'string') {
+    const fileName = type
+    const file = fs.readFileSync(path.resolve(fileName), 'utf8')
+    steps = JSON.parse(file)
+  } else {
+    steps = type
+  }
 
   // 檢查顏色
-  setInterval(() => {
+  const intervalCheck = setInterval(() => {
     hasColor(PURPLE_COLOR)
     hasColor(OTHER_USER_COLOR)
     // hasColor(USER_COLOR)
@@ -93,7 +99,7 @@ function replayStar(type = THUNDER_STAR) {
         if (step.state.toLowerCase() === 'up') return
 
         // 是攻擊技能、也不是 up 的話，根據冷卻時間隨機發動技能
-        const flow = Math.random() > 0.5 ? ['VK_A', 'VK_G'] : ['VK_G', 'VK_A']
+        const flow = Math.random() > 0.5 ? ['VK_A', 'VK_G', 'VK_Y'] : ['VK_G', 'VK_A', 'VK_Y']
         let everDo = false
         for (let aIndex = 0; aIndex < flow.length; aIndex++) {
           const skill = flow[aIndex]
@@ -112,12 +118,15 @@ function replayStar(type = THUNDER_STAR) {
 
         // 如果沒有發動過上述技能的話，有一個基本的
         if (!everDo) {
-          rb.keyToggle(keyMap['VK_V'], 'down')
-          setTimeout(() => rb.keyToggle(keyMap['VK_V'], 'up'), 130 + Math.random() * 50)
+          const basicKey = Math.random() > 0.9 ? 'VK_E' : 'VK_V'
+          rb.keyToggle(keyMap[basicKey], 'down')
+          setTimeout(() => rb.keyToggle(keyMap[basicKey], 'up'), 130 + Math.random() * 50)
         }
       })()
 
       finishedCount++
+
+      if (finishedCount % 100 === 0) console.log(finishedCount, step.index + 1)
       if (finishedCount === steps.length) finished()
     }, step.timestamp)
 
@@ -126,20 +135,10 @@ function replayStar(type = THUNDER_STAR) {
   }
 
   function finished() {
-    v.removeListener(listener)
+    console.log('finished!!', resolveFunction)
+    clearInterval(intervalCheck)
     resolveFunction()
   }
-
-  function listener(event) {
-    // 按下 esc 的時候終止, 不知道為什麼有時候會失效，應該是 stack 的問題
-    if (event.name === 'ESCAPE') {
-      timeoutList.forEach((timer) => clearTimeout(timer))
-      console.log('Dinner time!')
-
-      v.removeListener(listener)
-    }
-  }
-  v.addListener(listener)
 
   return promise
 }
@@ -150,4 +149,58 @@ async function go(times = 3) {
   }
 }
 
-export { replayStar, go }
+function anotherGo() {
+  const v = new GlobalKeyboardListener()
+  function listener(event) {
+    // 按下 esc 的時候終止, 不知道為什麼有時候會失效，應該是 stack 的問題
+    if (event.name === 'ESCAPE') {
+      timeoutList.forEach((timer) => clearTimeout(timer))
+      console.log('Dinner time!')
+      v.removeListener(listener)
+    }
+  }
+  v.addListener(listener)
+
+  // backup
+  // 'behavior/left-right-1.json'
+  // 'behavior/left-right-2.json'
+  // 'behavior/left-right-3.json'
+
+  const left = [
+    { from: 'left', to: 'top', file: './behavior/left-top-1.json', steps: null },
+    { from: 'left', to: 'top', file: './behavior/left-top-2.json', steps: null },
+    { from: 'left', to: 'top', file: './behavior/left-top-3.json', steps: null },
+    { from: 'left', to: 'bottom', file: './behavior/left-bottom-1.json', steps: null },
+    { from: 'left', to: 'bottom', file: './behavior/left-bottom-2.json', steps: null },
+  ]
+  const top = [
+    { from: 'top', to: 'left', file: './behavior/top-middle-1.json', steps: null },
+    { from: 'top', to: 'left', file: './behavior/top-middle-2.json', steps: null },
+  ]
+  const bottom = [
+    { from: 'bottom', to: 'left', file: './behavior/bottom-middle-1.json', steps: null },
+    { from: 'bottom', to: 'left', file: './behavior/bottom-middle-2.json', steps: null },
+  ]
+  const moveMap = { left, top, bottom }
+
+  Object.keys(moveMap).forEach((keys) => {
+    moveMap[keys].forEach((move) => {
+      const fileName = move.file
+      const fileContent = fs.readFileSync(fileName, 'utf8')
+      move.steps = JSON.parse(fileContent)
+    })
+  })
+
+  async function start(from = 'left') {
+    const index = Math.floor(Math.random() * moveMap[from].length)
+    const move = moveMap[from][index]
+    console.log('current move: ', move.file)
+    await replayStar(move.steps)
+
+    start(move.to)
+  }
+
+  start()
+}
+
+export { replayStar, go, anotherGo }
--- utils/start.js
+++ utils/start.js
@@ -1,6 +1,6 @@
 // 因為要在不同的系統開發的關係，這樣比較方便
 import { beforeStart, buy, displayMousePosition, extract } from './others.js'
-import { go } from './replay.js'
+import { anotherGo, go } from './replay.js'
 
 async function start() {
   await beforeStart(3)
@@ -8,8 +8,9 @@ async function start() {
   // displayMousePosition()
 
   // buy(8)
-  extract(70)
+  extract(40)
   // go()
+  // anotherGo()
 }
 
 export { start }
