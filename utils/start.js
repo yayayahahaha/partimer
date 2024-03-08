b6529dfefe531064fe76215562855d8b626e4663
flyc <yayayahahahaooii@gmail.com>
2024-03-08 14:43:35 +0800

    separate files

--- utils/start.js
+++ utils/start.js
@@ -1,6 +1,6 @@
 // 因為要在不同的系統開發的關係，這樣比較方便
 import { beforeStart, displayMousePosition, extract, market, ocean, marketAndExtract } from './others.js'
-import { anotherGo } from './replay.js'
+import { anotherGo } from './thunder.js'
 
 async function start() {
   await beforeStart(3)
