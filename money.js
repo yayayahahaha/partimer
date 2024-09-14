e7ca80da9470ba03d98502202083d2c29ebf767a
flyc <yayayahahahaooii@gmail.com>
2024-09-15 02:16:06 +0800

    tuning

--- money.js
+++ money.js
@@ -1,15 +1,11 @@
 import { MARKET_NO_MORE_STATUS } from './utils/money-utils.js'
 import { beforeStart, marketAndExtract } from './utils/others.js'
 
-async function start() {
-  await beforeStart(3)
+async function start(wait = true) {
+  wait && (await beforeStart(3))
 
-  recursiveMoney()
-
-  async function recursiveMoney() {
-    const { status } = await marketAndExtract()
-    if (status !== MARKET_NO_MORE_STATUS) recursiveMoney()
-  }
+  const { status } = await marketAndExtract()
+  if (status !== MARKET_NO_MORE_STATUS) await start(false)
 }
 
 start()
