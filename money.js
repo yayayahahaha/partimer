eb7d74d574b79d18a55e41fdb922b5d91f37c5c2
flyc <yayayahahahaooii@gmail.com>
2024-09-18 23:41:44 +0800

    add await beforeStart

--- money.js
+++ money.js
@@ -1,8 +1,8 @@
 import { MARKET_NO_MORE_STATUS, money } from './utils/money-utils.js'
 import { beforeStart, marketAndExtract } from './utils/others.js'
 
-function start() {
-  beforeStart(3)
+async function start() {
+  await beforeStart(3)
   money()
 }
 start()
